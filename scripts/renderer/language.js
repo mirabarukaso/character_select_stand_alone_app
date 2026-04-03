e3f5037143262c15ae0ea3ae482ea9306c0a28cb
mirabarukaso <79326992+mirabarukaso@users.noreply.github.com>
2026-04-03 16:01:59 +0900

    Update ComfyUI sampler list and order
    Expand and reorganize the SAMPLER_COMFYUI array: add new sampler variants (exp_heun_2_x0, exp_heun_2_x0_sde, dpmpp_2m_sde_heun, dpmpp_2m_sde_heun_gpu, gradient_estimation_cfg_pp, sa_solver, sa_solver_pece) and adjust ordering/formatting of existing entries for clarity.

--- scripts/renderer/language.js
+++ scripts/renderer/language.js
@@ -1,11 +1,11 @@
 import { callback_api_model_type, callback_regional_condition } from './callbacks.js';
  const CAT = '[Language]'
 
-export const SAMPLER_COMFYUI = ["euler_ancestral", "euler", "euler_cfg_pp", "euler_ancestral_cfg_pp", "heun", "heunpp2","dpm_2", "dpm_2_ancestral",
-    "lms", "dpm_fast", "dpm_adaptive", "dpmpp_2s_ancestral", "dpmpp_2s_ancestral_cfg_pp", "dpmpp_sde", "dpmpp_sde_gpu",
-    "dpmpp_2m", "dpmpp_2m_cfg_pp", "dpmpp_2m_sde", "dpmpp_2m_sde_gpu", "dpmpp_3m_sde", "dpmpp_3m_sde_gpu", "ddpm", "lcm",
-    "ipndm", "ipndm_v", "deis", "res_multistep", "res_multistep_cfg_pp", "res_multistep_ancestral", "res_multistep_ancestral_cfg_pp",
-    "gradient_estimation", "er_sde", "seeds_2", "seeds_3"];
+export const SAMPLER_COMFYUI = ["euler", "euler_cfg_pp", "euler_ancestral", "euler_ancestral_cfg_pp", "heun", "heunpp2", "exp_heun_2_x0", "exp_heun_2_x0_sde", "dpm_2", "dpm_2_ancestral",
+                  "lms", "dpm_fast", "dpm_adaptive", "dpmpp_2s_ancestral", "dpmpp_2s_ancestral_cfg_pp", "dpmpp_sde", "dpmpp_sde_gpu",
+                  "dpmpp_2m", "dpmpp_2m_cfg_pp", "dpmpp_2m_sde", "dpmpp_2m_sde_gpu", "dpmpp_2m_sde_heun", "dpmpp_2m_sde_heun_gpu", "dpmpp_3m_sde", "dpmpp_3m_sde_gpu", "ddpm", "lcm",
+                  "ipndm", "ipndm_v", "deis", "res_multistep", "res_multistep_cfg_pp", "res_multistep_ancestral", "res_multistep_ancestral_cfg_pp",
+                  "gradient_estimation", "gradient_estimation_cfg_pp", "er_sde", "seeds_2", "seeds_3", "sa_solver", "sa_solver_pece"];
 export const SCHEDULER_COMFYUI = ["normal", "karras", "exponential", "sgm_uniform", "simple", "ddim_uniform", "beta", "linear_quadratic", "kl_optimal"] ;
 
 export const  SAMPLER_WEBUI = ["DPM++ 2M", "DPM++ SDE", "DPM++ 2M SDE", "DPM++ 3M SDE", "DPM++ 2s a RF",
